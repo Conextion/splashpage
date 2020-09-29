279eaad190c76ce2e7c4114520d1d4ea7ce37539
Omri <ocohen@gmail.com>
2020-09-29 14:14:07 -0700

    build

--- splashpage/src/components/HeaderMarketing/constants.js
+++
@@ -1,9 +0,0 @@
-
-
-export const HEADER_NAV_ITEMS = [
-  
-];
-
-export const HEADER_MOBILE_NAV_ITEMS = [
-  
-];
--- splashpage/src/helpers/debounce.js
+++
@@ -1,8 +0,0 @@
-export function debounce(callback, time = 250) {
-  let timer;
-
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => callback(...args), time);
-  };
-}
