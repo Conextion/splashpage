50682091c3f89f80275bc982bfa667cce5e317b5
Omri <ocohen@gmail.com>
2020-09-29 14:11:34 -0700

    add build

---
+++ splashpage/build/precache-manifest.67467ec2de37903468f84bbf40867ec7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3962b313a7db707e6a91f99246113e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f62436cb56561c5dc6",
+    "url": "/static/css/main.64d755ad.chunk.css"
+  },
+  {
+    "revision": "1cbddf5285aaf8b986b0",
+    "url": "/static/js/2.8a400802.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/static/js/2.8a400802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f62436cb56561c5dc6",
+    "url": "/static/js/main.db7f85d9.chunk.js"
+  },
+  {
+    "revision": "a38385e4d27fa426d5c8",
+    "url": "/static/js/runtime-main.b04114d2.js"
+  },
+  {
+    "revision": "6d4e78225df0cfd5fe1bf3e8547fefe4",
+    "url": "/static/media/Lato-Regular.6d4e7822.ttf"
+  },
+  {
+    "revision": "6f37f64d6dc67efe01aa767ecf6360d7",
+    "url": "/static/media/Next.6f37f64d.svg"
+  },
+  {
+    "revision": "aa914a239322a7c4655117898b0a9287",
+    "url": "/static/media/authHeaderBg.aa914a23.svg"
+  },
+  {
+    "revision": "7f83ec968f7eee637e6c4f9c90232d6e",
+    "url": "/static/media/closeHamburgerIcon.7f83ec96.svg"
+  },
+  {
+    "revision": "17a1271877a135de1e9b9ad7f21a0926",
+    "url": "/static/media/conextion.17a12718.svg"
+  },
+  {
+    "revision": "20c189aa192501e4ec907d5f84e8fbb1",
+    "url": "/static/media/fa-solid-900.20c189aa.ttf"
+  },
+  {
+    "revision": "35cd610307861092016b5b2be6253b77",
+    "url": "/static/media/founders-grotesk-web-light.35cd6103.woff"
+  },
+  {
+    "revision": "387aa221f95c2d677d57e6fbc47e3f69",
+    "url": "/static/media/founders-grotesk-web-medium.387aa221.woff"
+  },
+  {
+    "revision": "60dce8c72bcff65f24cfa8d4c3211afc",
+    "url": "/static/media/founders-grotesk-web-regular.60dce8c7.woff"
+  },
+  {
+    "revision": "02fa648995184e22a6be1f17672de742",
+    "url": "/static/media/founders-grotesk-web-semibold.02fa6489.woff"
+  },
+  {
+    "revision": "d4422ce4005e7fba937d97106f5d802f",
+    "url": "/static/media/gamburger.d4422ce4.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/open_sans.629a55a7.ttf"
+  }
+]);
